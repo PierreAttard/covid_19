61198d7dc128c58ab81122898f0488f9da1406ac
PierreAttard <attard.pierre@gmail.com>
2020-03-28 18:00:19 +0100

    options main dropdown set from JH data

--- src/apps/layout_maker/main_data_lay.py
+++ src/apps/layout_maker/main_data_lay.py
@@ -28,20 +28,3 @@ def choice_data():
     ])
     return lay
 
-
-def graphe_country():
-
-    lay = \
-        [
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [4, 1, 2],
-                         'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [2, 4, 5],
-                         'type': 'bar', 'name': u'Montréal'},
-                    ]
-                }
-            )
-        ]
-    return lay
--- src/apps/main_page.py
+++ src/apps/main_page.py
@@ -1,11 +1,18 @@
+import pandas as pd
 from src.apps.layout_maker import main_layout as lyt
 from src.app import app
+import src.constantes as ct
 from dash.dependencies import Input, Output
 
 layout = lyt.main_lyt()
 
 @app.callback(
-    Output('app-1-display-value', 'children'),
-    [Input('app-1-dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    Output("choice-main-country", "options"),
+    [Input(ct.ID_DF_CCASES, "children")])
+def fill_options(data_death):
+    options = [{"label": "France", "value" : "France"}]
+    if data_death is None:
+        return options
+    countries = pd.read_json(data_death, orient='split').loc[:, ct.COUNTRY]
+    options = [{"label" : c, "value": c} for c in countries.drop_duplicates()]
+    return options
