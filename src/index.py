bb980a0ec49ec2931fa51e5deccb7ac4a3057a49
PierreAttard <attard.pierre@gmail.com>
2020-03-29 21:52:57 +0200

    change web file to run on startup

--- src/index.py
+++ src/index.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app import app
+from app import app, server
 from apps import main_page
 
 app.layout = html.Div([
